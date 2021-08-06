f54c7719d96f512149a61692c3306c4d9fd01f15
Titus Wormer <tituswormer@gmail.com>
2021-08-06 18:03:03 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import toString from 'mdast-util-to-string'
-import visit from 'unist-util-visit'
+import {toString} from 'mdast-util-to-string'
+import {visit} from 'unist-util-visit'
 import BananaSlug from 'github-slugger'
 
 const slugs = new BananaSlug()
